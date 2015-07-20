99bf1873cb8aa59735782643609cd73b96874781
Ahmed Awad <b00042183@aus.edu>
2015-07-21 00:31:56 +0400

    Commit pre changing shitup

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 import SemanticRouteMixin from 'semantic-ui-ember/mixins/application-route';
+import ResetScroll from '../mixins/reset-scroll';
 
-var ApplicationRoute = Ember.Route.extend(SemanticRouteMixin, {});
+var ApplicationRoute = Ember.Route.extend(ResetScroll,SemanticRouteMixin, {});
 
 export default ApplicationRoute;
